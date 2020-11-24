54b405882d42b62c842a24637f025ace7783c713
orhan <orhanu2020@gmail.com>
2020-11-24 11:30:59 -0500

    Open Closed

---
+++ src/B_OpenClosed.java
@@ -0,0 +1,57 @@
+public class B_OpenClosed {
+    /**
+     * O in SOLID: Open closed principle
+     * Your classes should be open for extension but close for modification
+     * meaning extension should not end up in modificaton.
+     * How: use abstract base classes and interfaces
+     */
+
+    //******************************************************************
+    /**
+     * Examples
+     */
+    //******************************************************************
+
+    interface IEmployee {
+        String getName();
+    }
+
+    class Employee implements IEmployee {
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    }
+
+    /**
+     * if we want to add new features to employee class.
+     * we can do so by adding new default function to IEmployee interface
+     */
+    interface IEmployee_Extended {
+        String getName();
+        default String getSurname() { return null; }
+    }
+
+
+
+    /**
+     * Using inheritance
+     */
+    class BaseEmployee {
+        public String getName() { return null; }
+    }
+
+    class Employee2 extends BaseEmployee {
+    }
+
+    /**
+     * if we want to add new features to Employee3 class.
+     * we can do so by adding new function to BaseEmployee class
+     */
+    class BaseEmployee_Extended {
+        public String getName() { return null; }
+        public String getSurname() { return null; }
+    }
+
+}
